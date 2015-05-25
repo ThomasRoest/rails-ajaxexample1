520c59a3d6fdaf8835cb2e9a32258015534ae059
Thomas Roest <thomasroest@outlook.com>
2015-05-25 10:44:04 +0000

    few edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,11 @@ class PostsController < ApplicationController
 
   def create
   	@post = Post.create(post_params)
-  	redirect_to posts_path
+    redirect_to posts_path
   end
 
   private
 
-	 
 	  def post_params
 	  	params.require(:post).permit(:title, :content)
 	  end
